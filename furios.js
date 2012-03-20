f2b9d08e8558196571186ce0cfd1a9aea5c50a56
Moritz Thielcke <moritz.thielcke@googlemail.com>
2012-03-20 09:02:20 +0000

    snap

--- furios.js
+++ furios.js
@@ -3,12 +3,26 @@ var furios = function(port){
     var server =  express.createServer();
     var jade = require('jade');
 
+
+    this.addView = function(path, template, fn){
+        console.log('bind to this path'+path);
+        server.get(path, function(req, res){
+            console.log('wtf?');
+            /*if(fn)
+                res.render(template, fn());
+            else
+                res.render(template);*/
+        });        
+        return this;
+    };
+
+
     this.bindResource = function(path, srcFile){
         delete require.cache['./resources/'+srcFile];    
         var controller = require('./resources/'+srcFile);
         if(controller.get){
-            console.log('GET '+path+' => resources/'+srcFile+'.get()');
-            server.get(path, function(req,res){
+            console.log('GET /REST'+path+' => resources/'+srcFile+'.get()');
+            server.get('/REST'+path, function(req,res){
                     controller = Object.create(controller); //is this required??:)
                     controller.inject(req,res);
                     var getResult = controller.get();
@@ -21,8 +35,6 @@ var furios = function(port){
                     //}
             });
         }
-
-
         return this;
     };
     
@@ -35,7 +47,12 @@ var furios = function(port){
     };
 };
 
+var myViewController = function(){
+    return { hello : 'world'};
+}
+
 new furios(process.env.C9_PORT)
     .bindResource('/hello','helloworld.js')
     .bindResource('/news', 'news.js')
+    .addView('/test\\.html', 'test.jade', myViewController)
     .run();
